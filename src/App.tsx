a5ec44222b62055f7009e89c8051952df51496ee
Bsd-123 <shira38801@gmail.com>
2026-02-25 01:22:11 +0200

    try2

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,6 @@ export const BASE_URL = "https://localhost:7291";
 function App()
 {
   
-=======
->>>>>>> 4f2870edf90c89ca3fc26234e2f3f29b1e26c9cf
-
-function App() {
-
->>>>>>> 236a4ab562dd2d7affec24b25e2586ddf8fd8a73
   return (
     <>
     <Provider  store={store}>
