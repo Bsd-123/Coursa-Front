24246a0da5e65a8c6c53d371f3067c1e0af4117a
Bsd-123 <shira38801@gmail.com>
2026-02-25 01:22:53 +0200

    try33

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Routes from './routes/routes'
 import './App.css'
 //import Login from './login'
-import Routes from './routes/routes'
 import { AuthProvider } from './auth/AuthContext'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
